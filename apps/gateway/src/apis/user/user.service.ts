207cd5aed95d6dbac1f52503a34210b3144b0f26
thursdaycurry <thursdaycurry@gmail.com>
2025-05-16 02:41:33 +0900

    create and apply gateway signin DTO

--- apps/gateway/src/apis/user/user.service.ts
+++ apps/gateway/src/apis/user/user.service.ts
@@ -5,9 +5,14 @@ import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class UserService {
   constructor(@Inject('AUTH_SERVICE') private authClient: ClientProxy) {}
+
   async signup(createUserDto) {
     const result = await firstValueFrom(this.authClient.send({ cmd: 'signup' }, createUserDto));
+    return result;
+  }
 
+  async signin(signinUserDto) {
+    const result = await firstValueFrom(this.authClient.send({ cmd: 'signin' }, signinUserDto));
     return result;
   }
 }
