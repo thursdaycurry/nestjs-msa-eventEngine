37ddb5e892bbca1d7d02cf847f4e6e8dab8155eb
thursdaycurry <thursdaycurry@gmail.com>
2025-05-16 02:42:58 +0900

    create APIs for signin and testing

--- apps/gateway/src/apis/user/user.controller.ts
+++ apps/gateway/src/apis/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { SigninUserDto } from './dto/signin-user.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +11,16 @@ export class UserController {
   @Post('signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     return await this.userService.signup(createUserDto);
+  }
+
+  @Post('signin')
+  async signin(@Body() signinUserDto: SigninUserDto) {
+    return await this.userService.signin(signinUserDto);
+  }
+
+  @Post('protected')
+  @UseGuards(JwtAuthGuard)
+  protected() {
+    return 'protected';
   }
 }
