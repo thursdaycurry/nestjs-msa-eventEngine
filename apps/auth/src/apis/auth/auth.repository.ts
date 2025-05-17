36a9c28000c068577918e15bdf60ed6ed7432ee4
thursdaycurry <thursdaycurry@gmail.com>
2025-05-17 20:17:40 +0900

    develop logic to store login history in authevents collection

--- apps/auth/src/apis/auth/auth.repository.ts
+++ apps/auth/src/apis/auth/auth.repository.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
+import { AuthEvent } from './schemas/authevent.schema';
 
 @Injectable()
 export class AuthRepository {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(AuthEvent.name)
+    private readonly authEventModel: Model<AuthEvent>,
   ) {}
 
   async findByEmail(email: string): Promise<User | null> {
@@ -32,5 +35,10 @@ export class AuthRepository {
 
   async updateUserRole(userId: string, newRole: string): Promise<User | null> {
     return this.userModel.findByIdAndUpdate(userId, { role: newRole });
+  }
+
+  async recordAuthEvent(authEventDto) {
+    const createdAuthEvent = new this.authEventModel(authEventDto);
+    return createdAuthEvent.save();
   }
 }
