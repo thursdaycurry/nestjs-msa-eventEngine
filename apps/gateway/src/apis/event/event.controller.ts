4995683de67e1a823176587179c8b20ff0fcfb9a
thursdaycurry <thursdaycurry@gmail.com>
2025-05-18 00:47:20 +0900

    set role guard to event, reward creation api controller

--- apps/gateway/src/apis/event/event.controller.ts
+++ apps/gateway/src/apis/event/event.controller.ts
@@ -1,26 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { EventService } from './event.service';
 import { CreateRewardItemDto } from './dto/create-reward-item.dto';
 import { CreateRewardDto } from './dto/create-reward.dto';
+import { UserRole } from 'src/common/constants/constants';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-  //TODO: add gards ROLES
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.OPERATOR)
   @Post()
   async createEvent(@Body() createEventDto: CreateEventDto) {
     return await this.eventService.createEvent(createEventDto);
   }
 
-  //TODO: add gards ROLES
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.OPERATOR)
   @Post('reward')
   async createReward(@Body() createRewardDto: CreateRewardDto) {
     return await this.eventService.createReward(createRewardDto);
   }
 
-  //TODO: add gards ROLES
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.OPERATOR)
   @Post('reward/item')
   async createRewardItem(@Body() createRewardItemDto: CreateRewardItemDto) {
     return await this.eventService.createRewardItem(createRewardItemDto);
